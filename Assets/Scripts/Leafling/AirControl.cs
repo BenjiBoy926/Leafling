f370ea2f9865588b30ff22bf7a97eeaa2ddeb015
Codey Huntting <creativecodey@gmail.com>
2024-06-07 00:42:05 -0500

    cleaned up air control

--- Assets/Scripts/Leafling/AirControl.cs
+++ Assets/Scripts/Leafling/AirControl.cs
@@ -16,15 +16,19 @@ namespace Leafling
 
         internal void ApplyTo(Rigidbody2D body, int applyDirection)
         {
-            float horizontalVelocity = body.GetVelocity(Dimension.X);
-            bool exceedsNegativeVelocity = horizontalVelocity <= -_topSpeed;
-            bool exceedsPositiveVelocity = horizontalVelocity >= _topSpeed;
-            if (applyDirection < 0 && exceedsNegativeVelocity || applyDirection > 0 && exceedsPositiveVelocity)
+            if (WillPushExceedTopSpeed(body, applyDirection))
             {
                 return;
             }
             Vector2 force = _acceleration * applyDirection * Vector2.right;
             body.AddForce(force);
+        }
+        private bool WillPushExceedTopSpeed(Rigidbody2D body, int applyDirection)
+        {
+            float horizontalVelocity = body.GetVelocity(Dimension.X);
+            bool exceedsNegativeVelocity = horizontalVelocity <= -_topSpeed;
+            bool exceedsPositiveVelocity = horizontalVelocity >= _topSpeed;
+            return (applyDirection < 0 && exceedsNegativeVelocity) || (applyDirection > 0 && exceedsPositiveVelocity);
         }
     }
 }
