b3d8a8b998c4a1fb4959ffd4d91100f30f892f5a
Codey Huntting <creativecodey@gmail.com>
2024-06-20 14:49:57 -0500

    more code cleanup

--- Assets/Scripts/Leafling/LeaflingSprite.cs
+++ Assets/Scripts/Leafling/LeaflingSprite.cs
@@ -12,11 +12,8 @@ namespace Leafling
         [SerializeField]
         private int _rightArmColorIndex = 2;
 
-        [Space]
-        [SerializeField, ReadOnly]
-        private Color _leftArmDefaultColor;
-        [SerializeField, ReadOnly]
-        private Color _rightArmDefaultColor;
+        private HueMapOperation_SetMultipleValues _setDesaturatedColors;
+        private HueMapOperation_SetMultipleValues _setDefaultColors;
 
         private void Reset()
         {
@@ -24,28 +21,28 @@ namespace Leafling
         }
         private void Awake()
         {
-            _leftArmDefaultColor = _map.GetValue(_leftArmColorIndex);
-            _rightArmDefaultColor = _map.GetValue(_rightArmColorIndex);
+            Color leftArmDefaultColor = _map.GetValue(_leftArmColorIndex);
+            Color rightArmDefaultColor = _map.GetValue(_rightArmColorIndex);
+            _setDesaturatedColors = new(
+                new HueMapOperation_SetValue(_leftArmColorIndex, DesaturateColor(leftArmDefaultColor)),
+                new HueMapOperation_SetValue(_rightArmColorIndex, DesaturateColor(rightArmDefaultColor)));
+            _setDefaultColors = new(
+                new HueMapOperation_SetValue(_leftArmColorIndex, leftArmDefaultColor),
+                new HueMapOperation_SetValue(_rightArmColorIndex, rightArmDefaultColor));
         }
 
         public void DesaturateArmColor()
         {
-            HueMapOperation_SetMultipleValues setValues = new(
-                new HueMapOperation_SetValue(_leftArmColorIndex, DesaturateColor(_leftArmDefaultColor)),
-                new HueMapOperation_SetValue(_rightArmColorIndex, DesaturateColor(_rightArmDefaultColor)));
-            _map.SetValues(setValues);
+            _map.SetValues(_setDesaturatedColors);
         }
         public void ResetArmColor()
         {
-            HueMapOperation_SetMultipleValues setValues = new(
-                new HueMapOperation_SetValue(_leftArmColorIndex, _leftArmDefaultColor),
-                new HueMapOperation_SetValue(_rightArmColorIndex, _rightArmDefaultColor));
-            _map.SetValues(setValues);
+            _map.SetValues(_setDefaultColors);
         }
 
         private Color DesaturateColor(Color color)
         {
-            Color.RGBToHSV(color, out float h, out float s, out float v);
+            Color.RGBToHSV(color, out float h, out _, out float v);
             return Color.HSVToRGB(h, 0, v);
         }
     }
