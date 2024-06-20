41e737c7c52ca0619db3035c3788c978f5f5b8f9
Codey Huntting <creativecodey@gmail.com>
2024-06-20 11:07:21 -0500

    set up a concise test that proves that the color set in code is not the same as the color shown by the shader

---
+++ Assets/Scripts/SetColorPropertyTest.cs
@@ -0,0 +1,22 @@
+using NaughtyAttributes;
+using UnityEngine;
+
+public class SetColorPropertyTest : MonoBehaviour
+{
+    [SerializeField]
+    private Renderer _renderer;
+    [SerializeField]
+    private Color _colorSetInCode;
+    [SerializeField]
+    private Color _colorShownByShader;
+
+    private void Reset()
+    {
+        _renderer = GetComponent<Renderer>();
+    }
+    [Button]
+    private void OnValidate()
+    {
+        _renderer.sharedMaterial.color = _colorSetInCode;
+    }
+}
