60b696b71011700c028edabc00ecc18554d1bdd8
Codey Huntting <creativecodey@gmail.com>
2024-06-24 21:00:32 -0500

    started the refactor of state machine

--- Assets/Scripts/Leafling/States/LeaflingStateMachine.cs
+++ Assets/Scripts/Leafling/States/LeaflingStateMachine.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,8 +7,53 @@ namespace Leafling
 {
     public class LeaflingStateMachine : MonoBehaviour
     {
+        [SerializeField]
+        private Leafling _leafling;
         private LeaflingState _currentState;
+        private List<LeaflingState> _stateList;
+        private Dictionary<Type, LeaflingState> _stateIndex;
 
+        private void Awake()
+        {
+            _stateList = LoadAllStates();
+            _stateIndex = BuildStateIndex(_stateList);
+        }
+        private List<LeaflingState> LoadAllStates()
+        {
+            return new()
+            {
+                new LeaflingState_Crouch(_leafling),
+                new LeaflingState_CrouchJump(_leafling),
+                new LeaflingState_Dash(_leafling, Vector2.zero, true),
+                new LeaflingState_DashAim(_leafling),
+                new LeaflingState_DashCancel(_leafling, Vector2.zero),
+                new LeaflingState_DashSquat(_leafling, Vector2.zero, true),
+                new LeaflingState_Drop(_leafling),
+                new LeaflingState_DropJump(_leafling),
+                new LeaflingState_Flutter(_leafling),
+                new LeaflingState_FreeFall(_leafling, FreeFallEntry.Normal),
+                new LeaflingState_Jump(_leafling),
+                new LeaflingState_JumpSquat(_leafling),
+                new LeaflingState_Landing(_leafling),
+                new LeaflingState_LongJump(_leafling),
+                new LeaflingState_Slide(_leafling),
+                new LeaflingState_Standing(_leafling),
+                new LeaflingState_WallJump(_leafling, CardinalDirection.Up),
+                new LeaflingState_WallSlide(_leafling, CardinalDirection.Up)
+            };
+        }
+        private static Dictionary<Type, LeaflingState> BuildStateIndex(List<LeaflingState> allStates)
+        {
+            Dictionary<Type, LeaflingState> result = new();
+            foreach (LeaflingState state in allStates)
+            {
+                result[state.GetType()] = state;
+            }
+            return result;
+        }
+
+        // TODO: refactor so that the caller sets the type of state, not the instance
+        // need to figure out how to handle setting info on the start before it enters though
         public void SetState(LeaflingState state)
         {
             if (_currentState != null)
