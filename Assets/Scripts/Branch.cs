c34cbc1072e902df0a09a23f3eca942acd4f9f62
Codey Huntting <creativecodey@gmail.com>
2025-01-25 16:57:00 -0600

    added a branch script
    it updates the width of the collider to match the width of the sprite, so we do not need to scale

---
+++ Assets/Scripts/Branch.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class Branch : MonoBehaviour
+{
+    [SerializeField]
+    private SpriteRenderer _renderer;
+    [SerializeField]
+    private BoxCollider2D _collider;
+    [SerializeField]
+    private float _spriteWidth = 2;
+    [SerializeField]
+    private float _colliderWidth = 1.2f;
+
+    private void Update()
+    {
+        if (!IsOperable())
+        {
+            return;
+        }
+        Vector2 size = _collider.size;
+        size.x = _renderer.size.x * (_colliderWidth / _spriteWidth);
+        _collider.size = size;
+    }
+    private bool IsOperable()
+    {
+        return _renderer != null && _collider != null;
+    }
+    private void Reset()
+    {
+        _renderer = GetComponent<SpriteRenderer>();
+        _collider = GetComponent<BoxCollider2D>();
+    }
+}
