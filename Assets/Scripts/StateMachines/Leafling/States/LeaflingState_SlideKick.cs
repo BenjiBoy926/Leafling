69ece384aa08e9c0900b4e5c5dc05465873565a3
Codey Huntting <147837412+PrincepsDraconum@users.noreply.github.com>
2025-12-20 17:09:36 -0600

    decent refactor but needs polish

--- Assets/Scripts/StateMachines/Leafling/States/LeaflingState_SlideKick.cs
+++ Assets/Scripts/StateMachines/Leafling/States/LeaflingState_SlideKick.cs
@@ -2,55 +2,26 @@ using UnityEngine;
 
 public class LeaflingState_SlideKick : LeaflingState
 {
-    private float TopSpeed => _airControl.ForwardTopSpeed;
-
     [SerializeField]
     private SpriteAnimation _animation;
     [SerializeField]
-    private float _gravityScale = 0.1f;
-    [SerializeField]
-    private DirectionalAirControl _airControl;
+    private float _upwardVelocity = 10;
 
     protected override void OnEnable()
     {
         base.OnEnable();
-        Target.SetHorizontalVelocity(0);
+        Target.SetVerticalVelocity(_upwardVelocity);
         Target.SetAnimation(_animation);
-        Target.SetGravityScale(_gravityScale);
-    }
-    protected override void OnDisable()
-    {
-        base.OnDisable();
-        Target.ResetGravityScale();
     }
     protected override void OnAnimationFinished()
     {
         base.OnAnimationFinished();
-        if (Target.IsCrouching)
-        {
-            Target.SendSignal(new LeaflingSignal<LeaflingState_Slide>());
-        }
-        else
-        {
-            Target.SendSignal(new LeaflingSignal_Landing(new LeaflingSignal<LeaflingState_Jump>()));
-        }
-    }
-    protected override void OnAnimationEnteredActionFrame()
-    {
-        base.OnAnimationEnteredActionFrame();
-        Target.SetHorizontalVelocity(TopSpeed * Target.FacingDirection);
+        Target.SendSignal(new LeaflingSignal_FreeFall(FreeFallEntry.Backflip));
     }
     protected override void OnDashTargetTouched(DashTarget target)
     {
         base.OnDashTargetTouched(target);
         target.Tickle(Target.DashTargeter);
-    }
-    protected override void Update()
-    {
-        base.Update();
-        if (HasEnteredActionFrame)
-        {
-            Target.ApplyAirControl(_airControl);
-        }
+        // Actually should attack
     }
 }
