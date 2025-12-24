b8f2af0d688b72badce442cfebb94d91370baa81
Codey Huntting <147837412+PrincepsDraconum@users.noreply.github.com>
2025-12-24 11:42:32 -0600

    it kind of works
    there is an issue when the aim points down where the reticle loops all the way around

--- Assets/Scripts/Leafling/DashReticle.cs
+++ Assets/Scripts/Leafling/DashReticle.cs
@@ -2,8 +2,20 @@
 
 public class DashReticle : MonoBehaviour
 {
+    [SerializeField]
+    private float _radiansPerSecond = 2 * Mathf.PI;
+    private float _currentAngle = 0;
+    private float _targetAngle;
+
     public void ShowAim(Vector2 aim)
     {
-        transform.up = aim;
+        _targetAngle = -Mathf.Atan2(aim.x, aim.y) * Mathf.Rad2Deg;
+    }
+
+    private void Update()
+    {
+        _currentAngle = Mathf.Lerp(_currentAngle, _targetAngle, _radiansPerSecond * Time.deltaTime);
+        Vector3 eulers = new(0, 0, _currentAngle);
+        transform.localEulerAngles = eulers;
     }
 }
