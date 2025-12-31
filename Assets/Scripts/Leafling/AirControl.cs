bccc1e91ebaee7a61704d95c67dc51f64d14d717
Codey Huntting <147837412+PrincepsDraconum@users.noreply.github.com>
2025-12-31 14:38:04 -0600

    resting force works ish
    as expected there is "jittering" at the 0 point

--- Assets/Scripts/Leafling/AirControl.cs
+++ Assets/Scripts/Leafling/AirControl.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 [Serializable]
 public struct AirControl
 {
+    private const float StationaryTolerance = 0.001f;
+
     public float TopSpeed => _topSpeed;
 
     [SerializeField]
@@ -14,26 +16,45 @@ public struct AirControl
 
     internal void ApplyTo(Rigidbody2D body, int applyDirection)
     {
-        if (applyDirection == 0)
-        {
-            return;
-        }
         if (WillPushExceedTopSpeed(body, applyDirection))
         {
             return;
         }
-        Vector2 force = _acceleration * applyDirection * Vector2.right;
+        int forceDirection = GetForceDirection(body, applyDirection);
+        Vector2 force = _acceleration * forceDirection * Vector2.right;
         body.AddForce(force);
-        ClampVelocity(body);
+        if (applyDirection != 0)
+        {
+            ClampVelocity(body);
+        }
     }
     // Codey: it is fine to enter this air control at a higher velocity than the 
     // top speed and let linear drag push the body back in range of the top speed
     private bool WillPushExceedTopSpeed(Rigidbody2D body, int applyDirection)
     {
+        if (applyDirection == 0) return false;
+
         float horizontalVelocity = body.GetVelocity(Dimension.X);
         bool exceedsNegativeVelocity = horizontalVelocity <= -_topSpeed;
         bool exceedsPositiveVelocity = horizontalVelocity >= _topSpeed;
         return (applyDirection < 0 && exceedsNegativeVelocity) || (applyDirection > 0 && exceedsPositiveVelocity);
+    }
+    private int GetForceDirection(Rigidbody2D body, int applyDirection)
+    {
+        return applyDirection == 0 ? GetRestingForceDirection(body) : applyDirection;
+    }
+    private int GetRestingForceDirection(Rigidbody2D body)
+    {
+        float xVelocity = body.GetVelocity(Dimension.X);
+        if (xVelocity > StationaryTolerance)
+        {
+            return -1;
+        }
+        else if (xVelocity < StationaryTolerance)
+        {
+            return 1;
+        }
+        return 0;
     }
     private void ClampVelocity(Rigidbody2D body)
     {
